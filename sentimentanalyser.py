fbb471f9bc4d8f68a3b765c8e45949b7336156ca
dd0 <de298@cam.ac.uk>
2017-11-22 13:28:01 +0000

    Fixed trivial classifier.

--- sentimentanalyser.py
+++ sentimentanalyser.py
@@ -1,6 +1,7 @@
 """Analyse tweets"""
 import webhandler
 
+
 class SentimentAnalyser(object):
     def __init__(self):
         self.negative_words = webhandler.get_negative_words()
@@ -8,14 +9,24 @@ class SentimentAnalyser(object):
         self.positive_words = webhandler.get_positive_words()
         self.companies = webhandler.get_company_info()
 
+        self.company_names = [c.name for c in self.companies]
+
     def analyse_tweet(self, tweet):
         """Analyse a tweet, extracting the subject and sentiment"""
         sentiment = 0
+        subject = None
 
         for word in tweet.split(" "):
             if word in self.positive_words:
                 sentiment = sentiment + 1
             if word in self.negative_words:
                 sentiment = sentiment - 1
+            if word in self.company_names:
+                subject = word
 
-        return [(self.companies[0].name, sentiment)]
+        if sentiment < 0:
+            sentiment = -1
+        elif sentiment > 0:
+            sentiment = 1
+
+        return [(subject, sentiment)]
